648d97af298816949073ff6e878b04816df9fb0b
Morris <tech.dev.user@whose.work>
2023-06-08 12:45:44 +0000

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import logging
+
+# logger create and loglevel setting.
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# main code
+def lambda_handler(event, context):
+
+    # data generation for response (dictionary type)
+    rtn_json = {"statusCode":"200", "urGlobalIPv4": str(event.get('source_ip')), "accessDate": str(datetime.utcnow().isoformat())}
+
+    # write execution logs to Cloudwatch
+    logger.info(rtn_json)
+
+    # response
+    return rtn_json
+
